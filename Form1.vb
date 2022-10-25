e9caba9a362890df0a43601e6796cb178f71063f
Richard Poate <info@zoople.com.au>
2022-10-26 00:01:49 +1100

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,55 @@
+﻿Public Class Form1
+
+    Private regKey As String = "HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\PageSetup"
+
+    Private keyHeader As String
+    Private keyFooter As String
+    Private keyBottom As String
+    Private keyLeft As String
+    Private keyRight As String
+    Private keyTop As String
+
+    Private Sub HtmlEditControl1_CommandsToolbarButtonClicked(sender As Object, e As Zoople.CommandsToolbarButtonClickedEventArgs) Handles HtmlEditControl1.CommandsToolbarButtonClicked
+
+        If e.ButtonIdentifier = "tsbPrint" Or e.ButtonIdentifier = "tsbPrintPreview" Then
+
+            e.Cancelled = True
+
+            'set temporary registry values
+            My.Computer.Registry.SetValue(regKey, "header", "")
+            My.Computer.Registry.SetValue(regKey, "footer", "")
+            My.Computer.Registry.SetValue(regKey, "margin_bottom", "0.50000")
+            My.Computer.Registry.SetValue(regKey, "margin_left", "0.50000")
+            My.Computer.Registry.SetValue(regKey, "margin_right", "0.50000")
+            My.Computer.Registry.SetValue(regKey, "margin_top", "0.50000")
+
+            Me.HtmlEditControl1.print_Document(True)
+
+
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        keyHeader = My.Computer.Registry.GetValue(regKey, "header", "no value")
+        keyFooter = My.Computer.Registry.GetValue(regKey, "footer", "no value")
+        keyBottom = My.Computer.Registry.GetValue(regKey, "margin_bottom", "no value")
+        keyLeft = My.Computer.Registry.GetValue(regKey, "margin_left", "no value")
+        keyRight = My.Computer.Registry.GetValue(regKey, "margin_right", "no value")
+        keyTop = My.Computer.Registry.GetValue(regKey, "margin_top", "no value")
+
+    End Sub
+
+    Private Sub Form1_Closed(sender As Object, e As EventArgs) Handles Me.Closed
+
+        'set registry values to original state
+        My.Computer.Registry.SetValue(regKey, "header", keyHeader)
+        My.Computer.Registry.SetValue(regKey, "footer", keyFooter)
+        My.Computer.Registry.SetValue(regKey, "margin_bottom", keyBottom)
+        My.Computer.Registry.SetValue(regKey, "margin_left", keyLeft)
+        My.Computer.Registry.SetValue(regKey, "margin_right", keyRight)
+        My.Computer.Registry.SetValue(regKey, "margin_top", keyTop)
+
+    End Sub
+End Class
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("RemovePrintURL")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Hewlett-Packard Company")>
+<Assembly: AssemblyProduct("RemovePrintURL")>
+<Assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d193741d-86ad-46a4-9d90-da97c4294d2b")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
